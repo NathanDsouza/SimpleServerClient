5bcbfc8ff57aa184f5d87caced9e4095f8a90331
Nathan <nathan.m.dsouza@gmail.com>
2020-06-24 16:51:23 -0400

    added in scripts

--- src/Client.py
+++ src/Client.py
@@ -1,24 +1,24 @@
 from socket import *
 import sys
 
-#def write_to_client_file():
-
-
-
 def open_udp(port, serverName, message):
     clientSocket = socket(AF_INET, SOCK_DGRAM)
     clientSocket.sendto("GET".encode(), (serverName, port))
     pastMessagesEncoded, addr = clientSocket.recvfrom(2048)
     pastMessages = pastMessagesEncoded.decode()
     f = open("client.txt", "a")
-    f.write("r_port: " + str(port) + "\n")
+    sentence = "r_port: " + str(port) + "\n"
+    f.write(sentence)
+    print(sentence, end='')
 
     while pastMessages != "No MSG":
         f.write(pastMessages)
+        print(pastMessages, end='')
         pastMessagesEncoded, addr = clientSocket.recvfrom(2048)
         pastMessages = pastMessagesEncoded.decode()
 
-    f.write(pastMessages + "\n")
+    f.write(pastMessages + "\n\n")
+    print(pastMessages)
     f.close()
     clientSocket.sendto(message.encode(), (serverName, port))
     clientSocket.close()
@@ -28,24 +28,28 @@ def create_file():
     f.close()
 
 
-if len(sys.argv) != 5:
-    print("you silly silly goose, <server_address> <n_port> <req_code> message")
-    quit()
+def main():
+    if len(sys.argv) != 5:
+        print("you silly silly goose, <server_address> <n_port> <req_code> message")
+        quit()
 
-serverAddr = sys.argv[1]
-nPort = int(sys.argv[2])
-reqCode = sys.argv[3]
-message = sys.argv[4]
+    serverAddr = sys.argv[1]
+    nPort = int(sys.argv[2])
+    reqCode = sys.argv[3]
+    message = sys.argv[4]
 
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverAddr, nPort))
-clientSocket.send(reqCode.encode())
-rPort = clientSocket.recv(1024).decode()
-print("From Server: ", rPort)
-if rPort != "0":
-    open_udp(int(rPort), serverAddr, message)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((serverAddr, nPort))
+    clientSocket.send(reqCode.encode())
+    rPort = clientSocket.recv(1024).decode()
+    if rPort != "0":
+        open_udp(int(rPort), serverAddr, message)
+    else:
+        print("Invalid req_code")
 
-exitKey = input("Bro click anything to shut it down fam")
-clientSocket.close()
+    exitKey = input("Click any key to exit") #raw_input needed for older python versions
+    clientSocket.close()
 
 
+if __name__ == "__main__":
+    main()
--- src/Server.py
+++ src/Server.py
@@ -2,9 +2,9 @@ from socket import *
 import sys
 
 
-def findOpenPort(serverSocket):
-    serverSocket.bind(('', 0))
-    return serverSocket.getsockname()[1]
+def findOpenPort(socket):
+    socket.bind(('', 0))
+    return socket.getsockname()[1]
 
 
 def append_to_messages(message, port):
@@ -19,19 +19,18 @@ def write_port_to_file(port):
     f.close()
 
 
-def send_past_messages(clientAddress, udpSocket):
-    print("fuck")
-
-
 def create_messages_file():
-    f = open("serverMessages.txt", "a")
+    f = open("serverMessages.txt", "w")
+    f.write("")
     f.close()
+
 
 def get_messages():
     f = open("serverMessages.txt")
     messages = f.readlines()
     f.close()
     return messages
+
 
 def open_udp(udpSocket, port):
     action, clientAddress = udpSocket.recvfrom(2048)
@@ -47,29 +46,34 @@ def open_udp(udpSocket, port):
     return 1 if messageDecoded == "TERMINATE" else 0
 
 
-if len(sys.argv) != 2:
-    print("you silly silly goose, input the req_code")
-    quit()
+def main():
+    if len(sys.argv) != 2:
+        print("you silly silly goose, input the req_code")
+        quit()
 
-reqCode = sys.argv[1]
-serverSocket = socket(AF_INET, SOCK_STREAM)
-nPort = findOpenPort(serverSocket)
-write_port_to_file(nPort)
-create_messages_file()
-serverSocket.listen(1)
-print("The server is ready to receive")
-while True:
-    connectionSocket, addr = serverSocket.accept()
-    givenCode = connectionSocket.recv(1024).decode()
-    if givenCode != reqCode:
-        message = "0"
-        print("GET OUT OF MA SWAMP")
-        connectionSocket.send(message.encode())
-    else:
-        udpSocket = socket(AF_INET, SOCK_DGRAM)
-        rPort = findOpenPort(udpSocket)
-        connectionSocket.send(str(rPort).encode())
-        if open_udp(udpSocket, rPort) == 1:
+    reqCode = sys.argv[1]
+    serverSocket = socket(AF_INET, SOCK_STREAM)
+    nPort = findOpenPort(serverSocket)
+    write_port_to_file(nPort)
+    create_messages_file()
+    serverSocket.listen(1)
+    print("SERVER_PORT: " + str(nPort))
+    while True:
+        connectionSocket, addr = serverSocket.accept()
+        givenCode = connectionSocket.recv(1024).decode()
+        if givenCode != reqCode:
+            message = "0"
+            connectionSocket.send(message.encode())
             break
-print("Closing Server")
-connectionSocket.close()
+        else:
+            udpSocket = socket(AF_INET, SOCK_DGRAM)
+            rPort = findOpenPort(udpSocket)
+            connectionSocket.send(str(rPort).encode())
+            if open_udp(udpSocket, rPort) == 1:
+                break
+    print("Closing Server")
+    connectionSocket.close()
+
+
+if __name__ == "__main__":
+    main()
